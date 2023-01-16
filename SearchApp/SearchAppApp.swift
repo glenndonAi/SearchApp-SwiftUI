b5886f53981a076d342b999e53b3c973353fcdb7
glenndon <>
2023-01-16 08:24:39 -0700

    swift project setup

---
+++ SearchApp/SearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppApp.swift
+//  SearchApp
+//
+//  Created by Glenndon on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
